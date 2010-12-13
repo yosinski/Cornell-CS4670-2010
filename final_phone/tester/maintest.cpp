31bf281eb7982770a8b47796f21a0ec3526e7d65
Cooper Bills <csb88@cornell.edu>
2010-12-12 23:13:09 -0500

    tester done

--- final_phone/tester/maintest.cpp
+++ final_phone/tester/maintest.cpp
@@ -13,16 +13,25 @@ int main( int argc, char** argv )
   cvNamedWindow( "result", 1 );
   for(int i = 1; i < argc; i++)
   {
-    IplImage* inputimg = cvLoadImage(argv[i]);
+    IplImage* inputimg = cvLoadImage(argv[i], CV_LOAD_IMAGE_COLOR);
     if(inputimg)
     {
-      IplImage* YUVimg = cvCloneImage(inputimg);
-      cvConvertImage(inputimg, YUVimg, CV_BGR2YCrCb); //phone use YUV
-      fd->processImage(YUVimg);
-      cvShowImage("result", YUVimg);
+      //IplImage* YUVimg = cvCreateImage(cvGetSize(inputimg), IPL_DEPTH_8U, inputimg->nChannels);
+      //cvConvertImage(inputimg, YUVimg, CV_BGR2YCrCb); //phone use YUV
+      //IplImage* fimg = cvCreateImage(cvGetSize(inputimg), IPL_DEPTH_32F, inputimg->nChannels);
+      //cvConvertScale(YUVimg, fimg, 1.0/255.0); //phone use YUV
+      //fd->processImage(fimg);
+      //cvConvertScale(fimg, YUVimg, 255);
+      //cvConvertImage(YUVimg, inputimg, CV_YCrCb2BGR); //phone use YUV
+      //cvShowImage("result", inputimg);
+      
+      fd->processImage2(inputimg);
+      cvShowImage("result", inputimg);
+      
       cvWaitKey(0);
-      cvReleaseImage(&YUVimg);
+      //cvReleaseImage(&YUVimg);
       cvReleaseImage(&inputimg);
+      //cvReleaseImage(&fimg);
     }else
       printf("Could Not Open Image: %s\n", argv[i]);
   }
